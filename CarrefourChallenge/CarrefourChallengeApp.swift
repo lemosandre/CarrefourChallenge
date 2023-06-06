f8933632bf2532c500d22d085e71b90674e73a7f
Andre Lemos <lemosandrecosta@gmail.com>
2023-06-06 11:45:54 -0300

    Initial Commit

---
+++ CarrefourChallenge/CarrefourChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarrefourChallengeApp.swift
+//  CarrefourChallenge
+//
+//  Created by Andre Lemos on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CarrefourChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
